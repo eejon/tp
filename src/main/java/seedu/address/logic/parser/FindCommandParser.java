cc2eb656e79994dba88c00b70c1b7668d9fb3fdd
eejon <eejonlee@gmail.com>
2026-03-12 11:28:01 +0800

    fix: Normalize whitespace in find command parser

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -19,7 +19,7 @@ public class FindCommandParser implements Parser<FindCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public FindCommand parse(String args) throws ParseException {
-        String trimmedArgs = args.trim();
+        String trimmedArgs = args.trim().replaceAll("\\s+", " ");
         if (trimmedArgs.isEmpty() || !trimmedArgs.matches("[a-zA-Z][a-zA-Z ]*")) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
